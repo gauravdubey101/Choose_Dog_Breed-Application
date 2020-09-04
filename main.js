376ed43a465401f4c1189a72cfa5494132ffb90b
Gaurav Dubey <gauravd638@gmail.com>
2020-09-04 09:31:25 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+async function start(){
+    //await will not run line untill now excute
+const response = await fetch("https://dog.ceo/api/breeds/list/all")
+const data = await response.json()
+//console.log(data)
+createBreedList(data.message)
+}
+
+start()
+
+
+function createBreedList(breedList){
+  document.getElementById("breed").innerHTML = `
+  <select onchange="loadByBreed(this.value)">
+        <option>Choose a dog breed </option>
+        ${Object.keys(breedList).map(function(breed){
+            return `<option>${breed}</options>`
+
+   }).join('')}
+  
+</select>
+`
+}
+
+async function loadByBreed(breed){
+    if(breed !="Choose a dog breed"){
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images
+        `)
+        const data = await response.json()
+    }
+
+}
